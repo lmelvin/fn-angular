73772afe56f2a6c3b942f11d342588cb56d4e461
Lucas Melvin <luke@lucasmelvin.com>
2017-08-02 00:20:32 -0500

    Added build for dist
    Added gulpfile for bundling dist js files

--- dist/fn-angular.js
+++ dist/fn-angular.js
@@ -40,3 +40,38 @@
         }
     }
 })()
+(function () {
+
+    angular.module("fn-angular-toggler", [])
+        .directive("fnToggler", fnToggler);
+
+    function fnToggler() {
+        return {
+            restrict: "A",
+            link: link
+        };
+
+        function link(scope, element, attrs) {
+
+            if (!angular.isDefined(attrs.fnToggler)) return;
+
+            var options = {
+                'animate': false
+            };
+
+            var toggler = new Foundation.Toggler($(element), options);
+
+            // init toggle depending on state of bool
+            if (scope.$eval(attrs.fnToggler)) {
+                toggler.toggle();
+            }
+
+            scope.$watch(attrs.fnToggler,
+                function (shouldCollapse) {
+                    if (angular.isDefined(shouldCollapse)) {
+                        toggler.toggle();
+                    }
+                });
+        }
+    }
+})()
